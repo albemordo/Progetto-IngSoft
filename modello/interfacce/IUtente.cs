9b02ec2a0b8e11c94d90d9d9a6b534b5ef10654f
Enrico Ghidoni <enricoghdn@gmail.com>
2018-09-04 21:20:38 +0200

    Aggiornata struttura progetto

---
+++ modello/interfacce/IUtente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutotrasportiFantini.modello.interfacce
+{
+    interface IUtente
+    {
+        String getIdAziendale();
+        void setIdAziendale(String id);
+        String getNome();
+        void setNome(String nome);
+        String getCognome();
+        void setCognome(String cognome);
+    }
+}
