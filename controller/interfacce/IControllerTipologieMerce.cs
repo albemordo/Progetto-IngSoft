b4490ff0548cf343d75b5753146c3fd54f400312
Enrico Ghidoni <enricoghdn@gmail.com>
2018-09-04 23:00:33 +0200

    Aggiunte interfacce controller

---
+++ controller/interfacce/IControllerTipologieMerce.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutotrasportiFantini.modello.interfacce;
+
+namespace AutotrasportiFantini.controller.interfacce
+{
+    interface IControllerTipologieMerce
+    {
+        List<ITipologiaMerce> ListaTipologieMerce();
+        ITipologiaMerce CreaTipologiaMerce(String tipologia);
+        ITipologiaMerce ModificaTipologiaMerce(ITipologiaMerce tipologia, String nomeTipologia);
+        void EliminaTipologiaMerce(ITipologiaMerce tipologia);
+    }
+}
--- modello/Indirizzo.cs
+++ modello/Indirizzo.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutotrasportiFantini.modello.interfacce;
 
-namespace AutotrasportiFantini.modello.interfacce
+namespace AutotrasportiFantini.modello
 {
     class Indirizzo : IIndirizzo
     {
