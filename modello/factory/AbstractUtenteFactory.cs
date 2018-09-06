d7fb34c0f1ed9b6c497bea92fc1b19ed33b6a5dd
mralby <albmord@gmail.com>
2018-09-06 23:00:23 +0200

    Aggiunta factory per gli utenti

---
+++ modello/factory/AbstractUtenteFactory.cs
@@ -0,0 +1,19 @@
+﻿using AutotrasportiFantini.modello.interfacce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutotrasportiFantini.modello.factory
+{
+    abstract class AbstractUtenteFactory
+    {
+        public abstract IAutista CreaAutista(string idAziendale, string nome, string cognome);
+
+        public abstract IDelegato CreaDelegato(string idAziendale, string nome, string cognome);
+
+        public abstract IResponsabile CreaResponsabile(string idAziendale, string nome, string cognome);
+
+    }
+}
---
+++ modello/factory/UtenteFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutotrasportiFantini.modello.interfacce;
+
+namespace AutotrasportiFantini.modello.factory
+{
+    class UtenteFactory : AbstractUtenteFactory
+    {
+        public override IAutista CreaAutista(string idAziendale, string nome, string cognome)
+        {
+            return new Autista(idAziendale, nome, cognome);
+        }
+
+        public override IDelegato CreaDelegato(string idAziendale, string nome, string cognome)
+        {
+            return new Delegato(idAziendale, nome, cognome);
+        }
+
+        public override IResponsabile CreaResponsabile(string idAziendale, string nome, string cognome)
+        {
+            return new Responsabile(idAziendale, nome, cognome);
+        }
+    }
+}
